c08104918fc0df9aa39939453fe81f95343128a2
RohitkdShetty <rohit.kavita.shetty@gmail.com>
2026-06-30 11:57:03 +0530

    Update project description in user_projects.jsx

--- reactjs_example/my_first_app/src/user_projects.jsx
+++ reactjs_example/my_first_app/src/user_projects.jsx
@@ -4,7 +4,7 @@ export const UserProjects = () => {
       <h1>My Learnings</h1>
       <h2>1. API Testing with Postman and JSON Server</h2>
       <h2>2. Selenium with Python</h2>
-      <h2>3.Basics of Manual Testing</h2>
+      <h2>For More View My Resume</h2>
     </div>
   );
 };
