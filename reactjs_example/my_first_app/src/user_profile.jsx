a6143ea74af6580e80d8fe14ce76e056c55f09cd
RohitkdShetty <rohit.kavita.shetty@gmail.com>
2026-06-30 11:58:01 +0530

    Refactor user profile component layout and content

--- reactjs_example/my_first_app/src/user_profile.jsx
+++ reactjs_example/my_first_app/src/user_profile.jsx
@@ -8,41 +8,32 @@ export const UserProfile = ({
   return (
     <div className="container">
 
-    <div className="stars"></div>
-    <div className="stars2"></div>
-    <div className="stars3"></div>
+      <div className="stars"></div>
+      <div className="stars2"></div>
+      <div className="stars3"></div>
 
-    <div className="profile-card">
+      <div className="profile-card">
 
-        {/* Image */}
         <img
-            src="/Rohit_tech.jpg"
-            alt="Rohit Shetty"
-            className="profile-image"
+          src="/Rohit_tech.jpg"
+          alt="Rohit Shetty"
+          className="profile-image"
         />
 
-        <h2>Hi, I am Rohit Shetty</h2>
+        <h2>HI, I am Rohit Shetty</h2>
 
         <p>Aspiring {profession}</p>
 
         <div className="stats">
-            <h3>I did my QA Internship at Sort Solutions</h3>
-            <h3>Internship Duration: {Internship} Months</h3>
+          <h3>I did my QA Internship at Sort Solutions</h3>
+          <h3><i>To Check out more about My Learnings, view my Resume</i></h3>
         </div>
 
-        <button
-            className="project-btn"
-            onClick={() => setShowProjects(!showProjects)}
-        >
-            {showProjects
-                ? "Hide Learnings"
-                : "My Learnings"}
-        </button>
+       
 
-        {showProjects && <UserProjects />}
-
-    </div>
-
-</div>
+          </div>
+          </div>
+        
+        
   );
 };
